47ca9b99139b343ca47594d1ff776e25c3371172
Kainatfatima711 <kainatfetima@gmail.com>
2026-02-08 14:11:25 +0500

    Add files via upload

---
+++ Salesman and Orders Table.sql
@@ -0,0 +1,14 @@
+DROP TABLE supplier;
+
+CREATE TABLE supplier(
+    SerialNo TEXT PRIMARY KEY,
+    SerialNAME TEXT,
+    Status INTEGER,
+    CITY TEXT
+);
+
+INSERT INTO supplier (SerialNo , SerialNAME , Status , CITY) VALUES
+('S1' , 'Smith' , 30 , 'London') ,
+('S5' , 'Adam' , 20 , 'Athens') ;
+
+SELECT * FROM supplier WHERE CITY = 'London';
